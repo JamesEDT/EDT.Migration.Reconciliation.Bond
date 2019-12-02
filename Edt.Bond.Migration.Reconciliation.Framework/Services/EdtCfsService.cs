97e1e6be9e148e145b3a7ff1fadda8b4513cfe9e
Tom <tjgould@hotmail.com>
2019-12-02 22:24:57 +1000

    add native logging

--- Edt.Bond.Migration.Reconciliation.Framework/Services/EdtCfsService.cs
+++ Edt.Bond.Migration.Reconciliation.Framework/Services/EdtCfsService.cs
@@ -1,5 +1,6 @@
 ﻿using Edt.Bond.Migration.Reconciliation.Framework.Models.EdtDatabase.Dto;
 using Edt.Bond.Migration.Reconciliation.Framework.Repositories;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -9,7 +10,9 @@ namespace Edt.Bond.Migration.Reconciliation.Framework.Services
     {
         public static long GetDocumentCountForBatch()
         {
-            var allFileLocations = EdtDocumentRepository.GetNativeFileLocations();
+            var allFileLocations = EdtDocumentRepository.GetNativeFileLocations().ToList();
+
+            LogNativeFileLocations(allFileLocations);
 
             var presentFileLocations = allFileLocations.Where(x => DoesDocumentExistInNativeStore(x));
 
@@ -18,13 +21,33 @@ namespace Edt.Bond.Migration.Reconciliation.Framework.Services
 
         private static bool DoesDocumentExistInNativeStore(DerivedFileLocation derivedFileLocation)
         {
-            var caseStoreLocation = Path.Combine(Settings.EdtCfsDirectory, $"Site01_Case{Settings.EdtCaseId.PadLeft(4, '0')}\\Docs");
-
-            var filePath = Path.Combine(caseStoreLocation, $"{derivedFileLocation.FolderId}\\{derivedFileLocation.DocumentId}\\{derivedFileLocation.Filename}");
+            var filePath = BuildDocFileName(derivedFileLocation);
 
             var exists =  File.Exists(filePath);
 
             return exists;
+        }
+
+        private static string BuildDocFileName(DerivedFileLocation derivedFileLocation)
+        {
+            var caseStoreLocation = Path.Combine(Settings.EdtCfsDirectory, $"Site01_Case{Settings.EdtCaseId.PadLeft(4, '0')}\\Docs");
+
+            var filePath = Path.Combine(caseStoreLocation, $"{derivedFileLocation.FolderId}\\{derivedFileLocation.DocumentId}\\{derivedFileLocation.Filename}");
+
+            return filePath;
+        }
+
+        private static void LogNativeFileLocations(List<DerivedFileLocation> derivedFiles)
+        {
+            using (var sw = new StreamWriter(".\\logs\\NativeComparison_DerivedLocations.csv"))
+            {
+                sw.WriteLine("DocumentId,Filename,FolderId,Full Path");
+
+                foreach (var file in derivedFiles)
+                {
+                    sw.WriteLine($"{file.DocumentId},{file.Filename},{file.FolderId},{BuildDocFileName(file)}");
+                }
+            }
         }
     }
 }
