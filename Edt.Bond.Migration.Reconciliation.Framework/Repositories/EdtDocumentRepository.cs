2c327bdc870954059f8d14749e1b0839ffa2101f
Michael Mullen <mickmuln@gmail.com>
2019-12-05 14:56:30 +1000

    Adds the quarantine test

--- Edt.Bond.Migration.Reconciliation.Framework/Repositories/EdtDocumentRepository.cs
+++ Edt.Bond.Migration.Reconciliation.Framework/Repositories/EdtDocumentRepository.cs
@@ -141,6 +141,18 @@ namespace Edt.Bond.Migration.Reconciliation.Framework.Repositories
 			  return rawLocations.ToDictionary(x => (string)x.DocumentNumber, x => (string)x.Location); 
         }
 
+        public static int GetDocumentQuarantineDocumentCount()
+        {
+	        var sql =
+		        $@"SELECT Count(document.DocumentId) FROM {GetDatabaseName()}.[Batch] batch 
+						INNER JOIN {GetDatabaseName()}.[Document] document ON batch.BatchID = document.BatchID
+						INNER JOIN {GetDatabaseName()}.[Folder] folder ON folder.FolderId = document.FolderID
+						WHERE batch.BatchName = '{Settings.EdtImporterDatasetName}' AND
+								folder.folderType = 1 --Quarantine Folder";
+
+	        return SqlExecutor.QueryFirstOrDefault<int>(sql);
+        }
+
 
 		private static string GetConnectionStringByName()
         {
@@ -154,6 +166,5 @@ namespace Edt.Bond.Migration.Reconciliation.Framework.Repositories
             return $"[eDiscoveryToolbox.Case.{caseId}].[dbo]";
 
         }
-
     }
 }
