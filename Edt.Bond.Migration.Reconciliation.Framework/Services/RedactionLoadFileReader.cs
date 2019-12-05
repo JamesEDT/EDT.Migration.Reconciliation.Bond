915e96932824ac14fb563bd7bfaf28ba85f2e570
Michael Mullen <mickmuln@gmail.com>
2019-12-05 16:16:19 +1000

    Adds the broken RedactedCountsAreEqualBetweenRedactionLoadFile test

---
+++ Edt.Bond.Migration.Reconciliation.Framework/Services/RedactionLoadFileReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace Edt.Bond.Migration.Reconciliation.Framework.Services
+{
+	public class RedactionLoadFileReader
+	{
+
+		public RedactionLoadFileReader()
+		{
+			using (var reader = new StreamReader(_path))
+			using (var csv = new CsvReader(reader))
+			{
+				csv.Configuration.Delimiter = ",";
+				csv.Configuration.Quote = '"';
+
+				csv.Configuration.BadDataFound = context =>
+				{
+					//ignore
+				};
+
+				csv.Configuration.HeaderValidated = null;
+				csv.Configuration.MissingFieldFound = null;
+
+				var records = csv.GetRecords<dynamic>();
+				string directory = Path.GetDirectoryName(_path);
+
+				var docs = records as dynamic[] ?? records.ToArray();
+			}
+		}
+	}
+}
