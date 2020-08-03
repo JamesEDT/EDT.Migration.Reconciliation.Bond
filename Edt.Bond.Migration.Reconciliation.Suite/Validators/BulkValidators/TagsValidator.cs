91ec0d4f07d43b42976c77389eedd5497b991acf
thomas-edt <TGould@edtfca.local>
2020-08-03 23:22:20 +0000

    Update TagsValidator.cs

--- Edt.Bond.Migration.Reconciliation.Suite/Validators/BulkValidators/TagsValidator.cs
+++ Edt.Bond.Migration.Reconciliation.Suite/Validators/BulkValidators/TagsValidator.cs
@@ -14,7 +14,7 @@ namespace Edt.Bond.Migration.Reconciliation.Suite.Validators
 {
     public class TagsValidator : BulkValidator, IDisposable
     {
-        private readonly string[] _unmigratedTags = new string[] { "Ems Folders:Deleted Items", ". LPP/Protected Review" };
+        private readonly string[] _unmigratedTags = new string[] { "Ems Folders:Deleted Items", " LPP/Protected Review" };
         private readonly List<Tag> _workbookRecords;
         private readonly StreamWriter _writer;
 
